8e2317554db3d68fd196ae2cb19a0fb10a6b3b55
rmdluo <rmdluo@gmail.com>
2022-12-02 10:29:47 -0500

    initial pyvis version

--- connections.py
+++ connections.py
@@ -4,6 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import string
 from unidecode import unidecode
+from pyvis.network import Network
 
 # parameters
 subject = 'math'
@@ -16,6 +17,7 @@ depth = 2
 # into an API call
 toQuery = lambda author : unidecode(author.replace(" ", "+"))
 toNormal = lambda author : unidecode(string.capwords(author.replace("+", " ")))
+toFilename = lambda author : author.replace(" ", "_").lower()
 url = lambda author : "https://export.arxiv.org/api/query?search_query=au:%s&cat=%s&max_results=%i" % (author, subject, breadth)
 
 graph = {toNormal(author) : set()} # author -> adjacent authors //// old author -> {adjacent authors -> num shared}
@@ -27,7 +29,7 @@ nextQueue = []
 for i in range(depth):
     while(len(queue) != 0):
         a = queue.pop(0)
-        data = urllib.request.urlopen(url(toQuery(a), subject)).read()
+        data = urllib.request.urlopen(url(toQuery(a))).read()
         feed = feedparser.parse(data)
 
         for entry in feed.entries:
@@ -51,8 +53,11 @@ for a1 in graph.keys():
         edges.append([a1, a2])
 
 visG.add_edges_from(edges)
-
+nt = Network('500px', '500px')
+nt.from_nx(visG)
+# nt.toggle_physics(True)
+nt.save_graph('%s_connections.html' % toFilename(author))
 # plots the graph using pyplot
-nx.draw_networkx(visG)
-plt.tight_layout()
-plt.show()
+# nx.draw_networkx(visG)
+# plt.tight_layout()
+# plt.show()
